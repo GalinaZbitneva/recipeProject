98ab7cfb0e839cf374c175c69e9a932d9d53136b
Galina Zbitneva <galinaknurenko@gmail.com>
2025-08-25 12:00:32 +0300

    added converters and commands folders with files.

---
+++ src/main/java/galka/recipes/converters/CategoryCommandToCategory.java
@@ -0,0 +1,24 @@
+package galka.recipes.converters;
+
+import galka.recipes.commands.CategoryCommand;
+import galka.recipes.domain.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category> {
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if(source == null){
+            return null;
+        }
+       final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+        return category;
+    }
+}
---
+++ src/main/java/galka/recipes/converters/NotesToNotesCommand.java
@@ -0,0 +1,25 @@
+package galka.recipes.converters;
+
+import galka.recipes.commands.NotesCommand;
+import galka.recipes.domain.Notes;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotesToNotesCommand implements Converter<Notes, NotesCommand> {
+    @Synchronized
+    @Nullable
+    @Override
+    public NotesCommand convert(Notes source) {
+        if (source == null) {
+            return null;
+        }
+
+        final NotesCommand notesCommand = new NotesCommand();
+        notesCommand.setId(source.getId());
+        notesCommand.setRecipeNotes(source.getRecipeNotes());
+        return notesCommand;
+    }
+}
