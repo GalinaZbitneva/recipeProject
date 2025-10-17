de774217f2376ea0139977eddf89e3b7584f733c
Galina Zbitneva <galinaknurenko@gmail.com>
2025-10-17 13:50:27 +0300

    Added image files, changed html file show

--- src/main/java/galka/recipes/controllers/ImageController.java
+++ src/main/java/galka/recipes/controllers/ImageController.java
@@ -1,9 +1,13 @@
 package galka.recipes.controllers;
 
 import galka.recipes.services.ImageService;
+import galka.recipes.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,9 +15,17 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageController {
 
     private  final ImageService imageService;
+    private final RecipeService recipeService;
 
-    public ImageController(ImageService imageService) {
+    public ImageController(ImageService imageService, RecipeService recipeService) {
         this.imageService = imageService;
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping("recipe/{id}/image")
+    public String showUpLoadForm(@PathVariable String id, Model model){
+        model.addAttribute("recipe",recipeService.findCommandById(Long.valueOf(id)));
+        return "recipe/imageuploadform";
     }
 
     @PostMapping("recipe/{id}/image")
@@ -21,7 +33,7 @@ public class ImageController {
         Long longId = Long.valueOf(id);
         imageService.saveImageFile(longId,file);
 
-                return "redirect:/recipe/" + id + "/show";
+                return "redirect:/recipe/show/" + id ;
     }
 
 }
--- src/test/java/galka/recipes/controllers/ImageControllerTest.java
+++ src/test/java/galka/recipes/controllers/ImageControllerTest.java
@@ -1,6 +1,8 @@
 package galka.recipes.controllers;
 
+import galka.recipes.commands.RecipeCommand;
 import galka.recipes.services.ImageService;
+import galka.recipes.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -14,14 +16,21 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class ImageControllerTest {
 
     @Mock
     ImageService imageService;
+
+    @Mock
+    RecipeService recipeService;
 
     ImageController controller;
 
@@ -31,9 +40,27 @@ class ImageControllerTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        controller = new ImageController(imageService);
+        controller = new ImageController(imageService,recipeService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
+
+    @Test
+    public void getImageForm() throws Exception {
+        //given
+        RecipeCommand command = new RecipeCommand();
+        command.setId(1L);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(command);
+
+        //when
+        mockMvc.perform(get("/recipe/1/image"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
+
+    }
+
 
     @Test
     void handleImagePost() throws Exception {
